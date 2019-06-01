710b2c644dabf021817522b89d3968b8e4728b32
jamieberrier <berrier.jamie@gmail.com>
2019-06-01 17:45:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ ORDER BY pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category FROM projects
+"SELECT projects.category, pledges.amount FROM projects
 JOIN
 WHERE projects.category = "music"
 GROUP BY"
