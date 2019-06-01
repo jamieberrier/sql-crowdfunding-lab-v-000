a4481e9daaaf7b7372496cdf3ad4f297e0357a77
jamieberrier <berrier.jamie@gmail.com>
2019-06-01 19:20:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,9 +21,8 @@ ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal AS diff FROM pledges
+"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal FROM pledges
 JOIN projects ON projects.id = pledges.project_id
-WHERE NOT diff < 0
 GROUP BY pledges.project_id
 ORDER BY projects.title"
 end
