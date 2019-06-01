ccd12b4ab857386452161af54e4785aa8e4f8753
jamieberrier <berrier.jamie@gmail.com>
2019-06-01 18:08:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,8 +27,8 @@ GROUP BY projects.id"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) FROM users
-JOIN pledges ON users.id = pledges.user_id
+"SELECT users.name, SUM(pledges.amount) FROM pledges
+JOIN users ON users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY pledges.amount, users.name"
 end
